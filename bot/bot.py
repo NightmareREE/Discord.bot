055db4d65c5076ce563e0465a419628007a57b77
NightmareREE <74125753+NightmareREE@users.noreply.github.com>
2021-01-29 18:27:15 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 from datetime import datetime
 import os
 import re
-import psycopg2
 bot = commands.Bot(command_prefix="s!", intents=discord.Intents.all())
 bot.remove_command('help')
 lastdel = {}
@@ -22,16 +21,6 @@ async def on_ready():
         print(member)
     await bot.change_presence(activity=discord.Game(name='Overwatch'))
     ##
-    DATABASE_URL = os.environ['DATABASE_URL']
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    cursor = conn.cursor()
-    create_table_query = '''CREATE TABLE points
-        (ID INTEGER PRIMARY KEY     NOT NULL
-        name    TEXT    NOT NULL
-        points  INT     NOR NULL);'''
-    cursor.execute(create_table_query)
-    conn.commit()
-    
 ########################################################################################################################
 
 @bot.event
