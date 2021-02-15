33cde1dedefa99530e19cfd9ad6b4c00ba38c8a6
NightmareREE <74125753+NightmareREE@users.noreply.github.com>
2021-02-15 14:39:51 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -289,8 +289,8 @@ async def on_message(message):
     user = c.fetchone()
 
     if user is None:
-        c.execute('INSERT INTO users(id, name, level, exp, rawexp, time, points) VALUES(%d,%s,%d,%d,%d,%s,%d)'%
-                  message.author.id, message.author.name, 1, 0, 0, time.time(), 0)
+        c.execute('INSERT INTO users(id, name, level, exp, rawexp, time, points) VALUES({0},{1},{2},{3},{4},{5},{6})'.format(
+                  message.author.id, message.author.name, 1, 0, 0, time.time(), 0))
         db.commit()
         return
 
