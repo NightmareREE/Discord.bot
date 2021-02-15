22131ce29642be4876b14a781d900fedc5a502e6
NightmareREE <74125753+NightmareREE@users.noreply.github.com>
2021-02-15 17:01:53 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -47,8 +47,7 @@ async def rank(ctx):
 
 
     c.execute(
-        "SELECT user.*, (SELECT count(*) FROM users AS members WHERE members.rawexp > user.rawexp) as Rank FROM users AS user WHERE id = %s",
-        (ctx.message.author.id,))
+        "SELECT users.*, (SELECT count(*) FROM users AS members WHERE members.rawexp > user.rawexp) as Rank FROM users AS user WHERE id=%s", (ctx.message.author.id,))
 
     user = c.fetchone()
 
