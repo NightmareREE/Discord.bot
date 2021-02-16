2e3c392a4f7ce72428aeec344d58ff61a0f1a61b
NightmareREE <74125753+NightmareREE@users.noreply.github.com>
2021-02-16 14:24:07 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -47,12 +47,13 @@ async def rank(ctx):
 
 
     c.execute('SELECT * FROM users WHERE id=%s', (ctx.message.author.id,))
-
     user = c.fetchone()
 
+    c.execute('SELECT 1 + count(*) AS rank FROM users WHERE rawexp > (SELECT rawexp FROM users WHERE id=%s', (ctx.message.author.id))
+    rank = c.fetchone
     out = discord.Embed(title='{}\'s Information'.format(ctx.message.author.name), color=0xff0000)
     out.set_thumbnail(url=ctx.message.author.avatar_url)
-    #out.add_field(name='Rank', value='#' + rank)
+    out.add_field(name='Rank', value='#' + rank)
     out.add_field(name='Level', value=user[2])
     out.add_field(name='EXP', value='{}/{}'.format(user[3], threshold(user[2])))
     out.add_field(name='Total EXP', value=user[4])
