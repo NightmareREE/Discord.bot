af70a2090e4a1c38ab0d8548ffdd67c4fb31864c
NightmareREE <74125753+NightmareREE@users.noreply.github.com>
2021-03-14 00:48:09 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -87,7 +87,7 @@ async def daily(ctx):
         await ctx.send(f"{ctx.message.author.mention} redeemed the daily bonus and won 1000 and now has {newpoints} points <:EZ:788447395805265990>")
     #else:
         #await ctx.send("You cant use this yet!")
-    await.ctx.send(user)
+    await ctx.send(user)
 ########################################################################################################################
 @bot.event
 async def on_message_delete(message):
