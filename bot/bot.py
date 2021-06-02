fb023c4d289a96642f3392c82c9f75ba26304b17
NightmareREE <74125753+NightmareREE@users.noreply.github.com>
2021-06-02 16:49:24 -0400

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -441,7 +441,7 @@ async def give(ctx, arg: User, money):
     giver = ctx.message.author.id
     taker = arg.id
     if(giver != taker):
-        oldpoints[0] = c.execute('SELECT points FROM users WHERE id=%s', (giver))
+        oldpoints = c.execute('SELECT points FROM users WHERE id=%s', (giver))[0]
         if(oldpoints < money):
             await ctx.send(f"{ctx.message.author.mention} You dont have enough money you broke ass")
         else:
